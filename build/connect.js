94218bb8efd64ec193e5f53c8fc1c3b68512f340
Victor Potasso <victorpotasso@gmail.com>
2018-05-03 23:43:26 +1200

    update just when value is different @ connect

--- build/connect.js
+++ build/connect.js
@@ -1,10 +1,16 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function connect(mapStateToProps, mapDispatchToProps) {
   const { subscribe, dispatch, getState } = window.__fluxo__;
@@ -15,8 +21,13 @@ function connect(mapStateToProps, mapDispatchToProps) {
       Component.prototype.render.call(this);
 
       subscribe(() => {
-        Container.prototype.props = _extends({}, Container.prototype.props, mapStateToProps(getState));
-        Component.prototype.render.call(this);
+        const newProps = mapStateToProps(getState);
+        const oldProps = _lodash2.default.pick(Container.prototype.props, Object.keys(newProps));
+        const isEqual = _lodash2.default.isEqual(oldProps, newProps);
+
+        Container.prototype.props = _extends({}, Container.prototype.props, newProps);
+
+        if (!isEqual) Component.prototype.render.call(this);
       });
     };
 
